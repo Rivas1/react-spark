f706ef12173cf4b95bb2c93815a767d67fec3aae
Rivas1 <rivas1@cpp.edu>
2019-06-07 14:35:22 -0700

    Added sample blog

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import Hidden from '@material-ui/core/Hidden';
 import Link from '@material-ui/core/Link';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core'
-import red from '@material-ui/core/colors/red'
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+import Container from '@material-ui/core/Container';
+import Markdown from './Markdown';
+import post1 from './blog-post.1.md';
+
 
 function MadeWithLove() {
   return (
@@ -28,140 +32,288 @@ function MadeWithLove() {
   );
 }
 
-const theme = createMuiTheme({
-	palette: {
-		type: 'dark',
-		primary: {
-			main: '#651fff'
-		},
-
-	}
-})
-
 const useStyles = makeStyles(theme => ({
-  icon: {
-    marginRight: theme.spacing(2),
+  toolbar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
   },
-  heroContent: {
-    
-    padding: theme.spacing(8, 0, 6),
+  toolbarTitle: {
+    flex: 1,
   },
-  heroButtons: {
-    marginTop: theme.spacing(4),
+  toolbarSecondary: {
+    justifyContent: 'space-between',
+    overflowX: 'auto',
   },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
+  toolbarLink: {
+    padding: theme.spacing(1),
+    flexShrink: 0,
+  },
+  mainFeaturedPost: {
+    position: 'relative',
+    backgroundColor: theme.palette.grey[800],
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing(4),
+    backgroundImage: 'url(https://source.unsplash.com/user/erondu)',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    backgroundColor: 'rgba(0,0,0,.3)',
+  },
+  mainFeaturedPostContent: {
+    position: 'relative',
+    padding: theme.spacing(3),
+    [theme.breakpoints.up('md')]: {
+      padding: theme.spacing(6),
+      paddingRight: 0,
+    },
+  },
+  mainGrid: {
+    marginTop: theme.spacing(3),
   },
   card: {
-    height: '100%',
     display: 'flex',
-    flexDirection: 'column',
+  },
+  cardDetails: {
+    flex: 1,
   },
   cardMedia: {
-    paddingTop: '56.25%', // 16:9
+    width: 160,
   },
-  cardContent: {
-    flexGrow: 1,
+  markdown: {
+    ...theme.typography.body2,
+    padding: theme.spacing(3, 0),
+  },
+  sidebarAboutBox: {
+    padding: theme.spacing(2),
+    backgroundColor: theme.palette.grey[200],
+  },
+  sidebarSection: {
+    marginTop: theme.spacing(3),
   },
   footer: {
-    
-    padding: theme.spacing(6),
+    backgroundColor: theme.palette.background.paper,
+    marginTop: theme.spacing(8),
+    padding: theme.spacing(6, 0),
   },
 }));
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const sections = [
+  'Technology',
+  'Design',
+  'Culture',
+  'Business',
+  'Politics',
+  'Opinion',
+  'Science',
+  'Health',
+  'Style',
+  'Travel',
+];
 
-export default function Album() {
+const featuredPosts = [
+  {
+    title: 'Featured post',
+    date: 'Nov 12',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+  },
+  {
+    title: 'Post title',
+    date: 'Nov 11',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+  },
+];
+
+const posts = [post1, post2, post3];
+
+const archives = [
+  'March 2020',
+  'February 2020',
+  'January 2020',
+  'December 2019',
+  'November 2019',
+  'October 2019',
+  'September 2019',
+  'August 2019',
+  'July 2019',
+  'June 2019',
+  'May 2019',
+  'April 2019',
+];
+
+const social = ['GitHub', 'Twitter', 'Facebook'];
+
+export default function Blog() {
   const classes = useStyles();
 
   return (
-     <MuiThemeProvider theme = {theme} > 
     <React.Fragment>
       <CssBaseline />
-     
-	      <AppBar position="relative">
-	        <Toolbar>
-	          <CameraIcon className={classes.icon} />
-	          <Typography variant="h6" color="inherit" noWrap>
-	            Album layout
-	          </Typography>
-	        </Toolbar>
-	      </AppBar>
-	  
-      <main>
-        {/* Hero unit */}
-        <div className={classes.heroContent}>
-          <Container maxWidth="sm">
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-              Album layout
-            </Typography>
-            <Typography variant="h5" align="center" color="textSecondary" paragraph>
-              Something short and leading about the collection below—its contents, the creator, etc.
-              Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-              entirely.
-            </Typography>
-            <div className={classes.heroButtons}>
-              <Grid container spacing={2} justify="center">
-                <Grid item>
-                  <Button variant="contained" color="primary">
-                    Main call to action
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button variant="outlined" color="primary">
-                    Secondary action
-                  </Button>
-                </Grid>
+      <Container maxWidth="lg">
+        <Toolbar className={classes.toolbar}>
+          <Button size="small">Subscribe</Button>
+          <Typography
+            component="h2"
+            variant="h5"
+            color="inherit"
+            align="center"
+            noWrap
+            className={classes.toolbarTitle}
+          >
+            Blog
+          </Typography>
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
+          <Button variant="outlined" size="small">
+            Sign up
+          </Button>
+        </Toolbar>
+        <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
+          {sections.map(section => (
+            <Link
+              color="inherit"
+              noWrap
+              key={section}
+              variant="body2"
+              href="#"
+              className={classes.toolbarLink}
+            >
+              {section}
+            </Link>
+          ))}
+        </Toolbar>
+        <main>
+          {/* Main featured post */}
+          <Paper className={classes.mainFeaturedPost}>
+            {/* Increase the priority of the hero background image */}
+            {
+              <img
+                style={{ display: 'none' }}
+                src="https://source.unsplash.com/user/erondu"
+                alt="background"
+              />
+            }
+            <div className={classes.overlay} />
+            <Grid container>
+              <Grid item md={6}>
+                <div className={classes.mainFeaturedPostContent}>
+                  <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+                    Title of a longer featured blog post
+                  </Typography>
+                  <Typography variant="h5" color="inherit" paragraph>
+                    Multiple lines of text that form the lede, informing new readers quickly and
+                    efficiently about what&apos;s most interesting in this post&apos;s contents.
+                  </Typography>
+                  <Link variant="subtitle1" href="#">
+                    Continue reading…
+                  </Link>
+                </div>
               </Grid>
-            </div>
-          </Container>
-        </div>
-        <Container className={classes.cardGrid} maxWidth="md">
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            {cards.map(card => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                    <Button size="small" color="primary">
-                      Edit
-                    </Button>
-                  </CardActions>
-                </Card>
+            </Grid>
+          </Paper>
+          {/* End main featured post */}
+          {/* Sub featured posts */}
+          <Grid container spacing={4} className={classes.cardGrid}>
+            {featuredPosts.map(post => (
+              <Grid item key={post.title} xs={12} md={6}>
+                <CardActionArea component="a" href="#">
+                  <Card className={classes.card}>
+                    <div className={classes.cardDetails}>
+                      <CardContent>
+                        <Typography component="h2" variant="h5">
+                          {post.title}
+                        </Typography>
+                        <Typography variant="subtitle1" color="textSecondary">
+                          {post.date}
+                        </Typography>
+                        <Typography variant="subtitle1" paragraph>
+                          {post.description}
+                        </Typography>
+                        <Typography variant="subtitle1" color="primary">
+                          Continue reading...
+                        </Typography>
+                      </CardContent>
+                    </div>
+                    <Hidden xsDown>
+                      <CardMedia
+                        className={classes.cardMedia}
+                        image="https://source.unsplash.com/random"
+                        title="Image title"
+                      />
+                    </Hidden>
+                  </Card>
+                </CardActionArea>
               </Grid>
             ))}
           </Grid>
-        </Container>
-      </main>
+          {/* End sub featured posts */}
+          <Grid container spacing={5} className={classes.mainGrid}>
+            {/* Main content */}
+            <Grid item xs={12} md={8}>
+              <Typography variant="h6" gutterBottom>
+                From the Firehose
+              </Typography>
+              <Divider />
+              {posts.map(post => (
+                <Markdown className={classes.markdown} key={post.substring(0, 40)}>
+                  {post}
+                </Markdown>
+              ))}
+            </Grid>
+            {/* End main content */}
+            {/* Sidebar */}
+            <Grid item xs={12} md={4}>
+              <Paper elevation={0} className={classes.sidebarAboutBox}>
+                <Typography variant="h6" gutterBottom>
+                  About
+                </Typography>
+                <Typography>
+                  Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit
+                  amet fermentum. Aenean lacinia bibendum nulla sed consectetur.
+                </Typography>
+              </Paper>
+              <Typography variant="h6" gutterBottom className={classes.sidebarSection}>
+                Archives
+              </Typography>
+              {archives.map(archive => (
+                <Link display="block" variant="body1" href="#" key={archive}>
+                  {archive}
+                </Link>
+              ))}
+              <Typography variant="h6" gutterBottom className={classes.sidebarSection}>
+                Social
+              </Typography>
+              {social.map(network => (
+                <Link display="block" variant="body1" href="#" key={network}>
+                  {network}
+                </Link>
+              ))}
+            </Grid>
+            {/* End sidebar */}
+          </Grid>
+        </main>
+      </Container>
       {/* Footer */}
       <footer className={classes.footer}>
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-          Something here to give the footer a purpose!
-        </Typography>
-        <MadeWithLove />
+        <Container maxWidth="lg">
+          <Typography variant="h6" align="center" gutterBottom>
+            Footer
+          </Typography>
+          <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+            Something here to give the footer a purpose!
+          </Typography>
+          <MadeWithLove />
+        </Container>
       </footer>
       {/* End footer */}
     </React.Fragment>
-    </MuiThemeProvider>
   );
 }
